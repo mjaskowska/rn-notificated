555b46e475815e4e7a1977041c677fca3e3c220a
Magda <jaskowska.magda@gmail.com>
2022-05-19 12:05:24 +0100

    update demo.tsx - prepare boilerplate

--- src/screens/Demo.tsx
+++ src/screens/Demo.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import {Image, SafeAreaView, StyleSheet} from 'react-native';
 import {Button} from '../components/Button';
-import Logo from '../assets/rnn-logo.png'
+import Logo from '../assets/rnn-logo.png';
 
 export const Demo = () => {
   return (
     <SafeAreaView style={S.mainView}>
       <Button label="Emit notification" variant="success" />
-      {/* <Button
+      <Image source={Logo} style={S.logo} />
+    </SafeAreaView>
+  );
+};
+
+const S = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    borderWidth: 1,
+  },
+  logo: {
+    position: 'absolute',
+    top: 50,
+    right: -20,
+    width: 350,
+    height: 350,
+    opacity: 0.5,
+    transform: [{rotate: '-10deg'}],
+  },
+});
+
+{
+  /* <Button
         label="Emit info"
         variant="info"
         onPress={() =>
@@ -33,25 +56,5 @@ export const Demo = () => {
             params: {title: 'Hello!', description: 'Some text goes here'},
           })
         }
-      /> */}
-      <Image source={Logo} style={S.logo}/>
-    </SafeAreaView>
-  );
-};
-
-const S = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    borderWidth: 1,
-  },
-  logo: {
-    position: 'absolute',
-    top: 50,
-    right: -20,
-    width: 350,
-    height: 350,
-    opacity: 0.5,
-    transform: [{rotate: '-10deg'}]
-  }
-});
+      /> */
+}
