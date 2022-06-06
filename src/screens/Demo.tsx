355db45edce32de6a9c899cc63ccd75689e91bc0
Magda <jaskowska.magda@gmail.com>
2022-06-06 09:53:40 +0200

    center logo

--- src/screens/Demo.tsx
+++ src/screens/Demo.tsx
@@ -20,11 +20,11 @@ const S = StyleSheet.create({
   },
   logo: {
     position: 'absolute',
-    top: 50,
-    right: -20,
+    top: 100,
+    right: 20,
     width: 350,
     height: 350,
-    opacity: 0.5,
+    opacity: 0.4,
     transform: [{rotate: '-10deg'}],
   },
 });
