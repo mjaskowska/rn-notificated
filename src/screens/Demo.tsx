a2c719ecb4e6860ceab499d1cf8c12fef1c9c578
Magda <jaskowska.magda@gmail.com>
2022-06-09 20:01:14 +0200

    update helpers

--- src/screens/Demo.tsx
+++ src/screens/Demo.tsx
@@ -28,33 +28,3 @@ const S = StyleSheet.create({
     transform: [{rotate: '-10deg'}],
   },
 });
-
-{
-  /* <Button
-        label="Emit info"
-        variant="info"
-        onPress={() =>
-          notify('info', {
-            params: {title: 'Hello!', description: 'Some text goes here'},
-          })
-        }
-      />
-      <Button
-        label="Emit error"
-        variant="error"
-        onPress={() =>
-          notify('error', {
-            params: {title: 'Hello!', description: 'Some text goes here'},
-          })
-        }
-      />
-      <Button
-        label="Emit warning"
-        variant="warning"
-        onPress={() =>
-          notify('warning', {
-            params: {title: 'Hello!', description: 'Some text goes here'},
-          })
-        }
-      /> */
-}
